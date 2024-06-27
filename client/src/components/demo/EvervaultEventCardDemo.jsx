e783312d9fa3c0d097a225aa43e224d28f87aa05
parthmern <parthmern@gmail.com>
2024-06-27 08:37:55 -0400

    small change

--- client/src/components/demo/EvervaultEventCardDemo.jsx
+++ client/src/components/demo/EvervaultEventCardDemo.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import moment from "moment";
 import { EvervaultCard, Icon } from "../ui/evervault-card";
 
 export function EvervaultCardDemo({event}) {
@@ -14,8 +15,8 @@ export function EvervaultCardDemo({event}) {
       <h2 className="text-white capitalize productFont mt-4 text-sm font-light">
         {event?.desc}
       </h2>
-      <p className="text-sm border font-light productFont border-white/[0.2] rounded-full mt-4 text-white px-2 py-0.5">
-        Read more  
+      <p className="text-sm border font-light productFont bg-white bg-opacity-10 border-white/[0.2] rounded-full mt-4 text-white px-2 py-0.5">
+      {moment(event?.createdAt).format('MMMM D, YYYY')}  
       </p>
     </div>
   );
