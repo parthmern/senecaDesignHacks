e6783267881262fcf85eeb6904984333c44a4956
parthmern <parthmern@gmail.com>
2024-06-28 00:06:21 -0400

    single wp page setup

--- client/src/components/pages/SingleWorkshopPage.jsx
+++ client/src/components/pages/SingleWorkshopPage.jsx
@@ -1,12 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom';
+import { apiConnector } from '../../services/apiConnector';
+import { url } from '../../services/apis';
 
 const SingleWorkshopPage = () => {
 
     const {id} = useParams();
 
+    const [wp, setWp] = useState(null);
+
+    async function getDetailsOfSingleWorkshop(){
+        const res = await apiConnector("POST", url + "workshop/getDetailsOfSingleWorkshop", {id} );
+        console.log(res);
+        setWp(res?.data?.wpDetails[0]);
+    }
+
+    useEffect(()=>{
+        getDetailsOfSingleWorkshop();
+    }, []);
+
   return (
-    <div>SingleWorkshopPage {id}</div>
+    <div className='text-white'>SingleWorkshopPage {id}</div>
   )
 }
 
