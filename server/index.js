c5b092cdcaa6fd7ccc09cfe80b4e3fc57501ce00
parthmern <parthmern@gmail.com>
2024-06-30 00:01:39 -0400

    fixing j

--- server/index.js
+++ server/index.js
@@ -58,6 +58,17 @@ const dbConnect = require("./config/dbConnect");
 // first middlewares
 app.use(express.json());
 
+app.use((req, res, next) => {
+    res.header(
+      "Access-Control-Allow-Origin",
+      "http://localhost:5173/"
+    );
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    res.header("Access-Control-Allow-Headers", "Content-Type");
+    next();
+  });
+  
+
 // body parser
 const bodyParser = require("body-parser");
 app.use(bodyParser.json());
