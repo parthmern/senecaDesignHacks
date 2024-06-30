ecbce425eefe33a380996418512ad675baccbaab
parthmern <parthmern@gmail.com>
2024-06-29 23:27:11 -0400

    fixing b

--- server/index.js
+++ server/index.js
@@ -23,16 +23,17 @@ const cors = require("cors");
 const corsOptions = {
     // origin: 'http://localhost:5173',
     // credentials: true,
-    // methods: 'GET, POST, PUT, DELETE, OPTIONS',
-    // allowedHeaders: 'Content-Type, Authorization',
+    methods: 'GET, POST, PUT, DELETE, OPTIONS',
+    allowedHeaders: 'Content-Type, Authorization',
 
-    origin:'http://localhost:5173', 
-    // credentials:true,            //access-control-allow-credentials:true
-    // optionSuccessStatus:200
+    origin:'http://localhost:5173/', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
 };
 
 
-app.use(cors(corsOptions));
+app.options('*', cors(corsOptions)); // Enable preflight across all routes
+
 
 const fileUpload = require("express-fileupload");
 app.use(
